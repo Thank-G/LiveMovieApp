d7cde27ba4f39cb6e16c6b1c79dda272f74c14d8
Thank-G <praisejahwithmetb@gmail.com>
2020-02-13 07:30:47 +0100

    movie list page and the routings added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +18,7 @@ import { MovieListComponent } from './components/movie-list/movie-list.component
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
